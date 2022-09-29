16b18173c3bf6ef7b1f5de9736579a1708dc3904
YuboMao <mao00071@morris.umn.edu>
2022-09-29 16:18:42 -0500

    create method

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -3,5 +3,16 @@
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+	int i, j;
+	char data[100] = {0};
+
+	strcpy(data, s);
+	for(i=0, j=0; data[i]!='\0'; i++){
+		if (data[i] = 'a'/'e'/'i'/'o'/'u'){
+			data[j++] = data[i];
+		}
+	}
+	data[j] = '\0';
+	printf("%s\n", data);
+	return (char*) "";
 }
