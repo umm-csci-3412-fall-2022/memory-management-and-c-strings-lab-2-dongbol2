3a9cbeecdfdd2f1ab7e67f6650cb6db169c6837d
YuboMao <mao00071@morris.umn.edu>
2022-09-29 22:45:06 -0500

    the dismevowel.c can work correctly.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,18 +1,38 @@
 #include <stdlib.h>
-
+#include <string.h>
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
 	int i, j;
 	char data[100] = {0};
 
-	strcpy(data, s);
+	strcpy(data, str);
 	for(i=0, j=0; data[i]!='\0'; i++){
-		if (data[i] = 'a'/'e'/'i'/'o'/'u'){
+		if (data[i] != 'a'){
+			data[j++] = data[i];
+		}
+	}
+	for(i=0, j=0; data[i]!='\0'; i++){
+		if (data[i] != 'e'){
+			data[j++] = data[i];
+		}
+	}
+	for(i=0, j=0; data[i]!='\0'; i++){
+		if (data[i] != 'i'){
+			data[j++] = data[i];
+		}
+	}
+	for(i=0, j=0; data[i]!='\0'; i++){
+		if (data[i] != 'o'){
+			data[j++] = data[i];
+		}
+	}
+	for(i=0, j=0; data[i]!='\0'; i++){
+		if (data[i] != 'u'){
 			data[j++] = data[i];
 		}
 	}
 	data[j] = '\0';
-	printf("%s\n", data);
-	return (char*) "";
+	char *ret = data;
+	return (ret);
 }
--- disemvowel/main.c
+++ disemvowel/main.c
@@ -13,4 +13,5 @@ int main(int argc, char *argv[]) {
   while (getline(&line, &size, stdin) > 0) {
     printf("%s\n", disemvowel(line));
   }
+  free(line);
 }
